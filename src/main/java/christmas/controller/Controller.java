b714694b112e5d1fbea5e05dabb4a72f08edbfbc
gusdn0108 <gusdn6671@naver.com>
2023-11-09 17:09:34 +0900

    feat(Controller) : part1 제작

--- src/main/java/christmas/controller/Controller.java
+++ src/main/java/christmas/controller/Controller.java
@@ -1,7 +1,18 @@
 package christmas.controller;
 
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
 public class Controller {
     public void run() {
-        System.out.println("hello controller");
+        part1();
+    }
+
+    private void part1(){
+        OutputView.printEventStart();
+        OutputView.printExpectationVisited();
+        String userInput = InputView.userInput();
+        InputView.parsedInputNumber(userInput);
+
     }
 }
