405e02c926673c273d9e78dba675d61af48fdb95
gusdn0108 <gusdn6671@naver.com>
2023-11-09 17:18:43 +0900

    feat(AppetizerMenu) : 애피타이저 매뉴제작

--- src/main/java/christmas/domain/menu/AppetizerMenu.java
+++ src/main/java/christmas/domain/menu/AppetizerMenu.java
@@ -1,4 +1,17 @@
 package christmas.domain.menu;
 
-public class AppetizerMenu {
+public enum AppetizerMenu {
+    MUSHROOMCREAMSOUP("양송이수프", 6000),
+    TAPAS("타파스", 5500),
+    CAESARSALAD("시저샐러드",8000);
+
+    private final String name;
+    private final int price;
+
+
+    AppetizerMenu(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
 }
+
