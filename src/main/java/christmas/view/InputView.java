eb1bac57e052ed18f3ff5e2723d7ca0fa17f50d7
gusdn0108 <gusdn6671@naver.com>
2023-11-09 17:08:56 +0900

    feat(InputView) : 사용자 입력 값 만들기 / 사용자 입력 값 parsedInt 만들기

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,4 +1,14 @@
 package christmas.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+    public static String userInput(){
+        return Console.readLine();
+    }
+
+    public static int parsedInputNumber(String input){
+        return Integer.parseInt(input);
+    }
 }
