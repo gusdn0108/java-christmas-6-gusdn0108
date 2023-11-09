8f0ee0a22374f96d53bcd3c9c92b70c9f86f669b
gusdn0108 <gusdn6671@naver.com>
2023-11-09 17:18:57 +0900

    feat(DessertMenu) : 디저트 매뉴제작

--- src/main/java/christmas/domain/menu/DessertMenu.java
+++ src/main/java/christmas/domain/menu/DessertMenu.java
@@ -1,4 +1,15 @@
 package christmas.domain.menu;
 
 public enum DessertMenu {
+    CHOCOLATECAKE("초코케이크", 15000),
+    ICECREAM("아이스크림", 5000);
+
+    private final String name;
+    private final int price;
+
+
+    DessertMenu(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
 }
