2d19a4a1b3b224c7df9107f8ce74527fd9e703cb
gusdn0108 <gusdn6671@naver.com>
2023-11-12 23:14:20 +0900

    feat(Controller) : OderMenu를 반환하는 주문로직 만들기

--- src/main/java/christmas/controller/Controller.java
+++ src/main/java/christmas/controller/Controller.java
@@ -27,7 +27,23 @@ public class Controller {
         return InputView.userInput();
     }
 
-    
+    private OrderMenu foodOrder(String order) {
+        Map<Menu, Integer> orderMap = new HashMap<>();
+        for (String item : order.split(",")) {
+            String[] parts = item.split("-");
+            if (parts.length != 2) {
+                throw new IllegalArgumentException("매뉴와 갯수를 입력해주세요");
+            }
+            Menu menu = Menu.findByName(parts[0].trim());
+            int count = Integer.parseInt(parts[1].trim());
+
+            if(orderMap.containsKey(menu)){
+                throw new IllegalArgumentException("같은 매뉴를 입력했습니다");
+            }
+            orderMap.put(menu,count);
+        }
+        return new OrderMenu(orderMap);
+    }
 
 
 
