e6f35929f9882815ecd0c634e7b88a126f6db0f9
gusdn0108 <gusdn6671@naver.com>
2023-11-14 03:54:24 +0900

    refactor (InputView) : ErrorMessage 클래스 사용

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 import christmas.domain.Menu;
 import christmas.domain.OrderMenu;
 import christmas.domain.VisitDay;
+import christmas.utils.ErrorMessage;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,35 +25,53 @@ public class InputView {
             String userInput = InputView.userInput();
             return new VisitDay(InputView.parsedInputNumber(userInput));
         } catch (IllegalArgumentException e) {
-            System.out.println("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+            System.out.println(ErrorMessage.USER_INSERT_NOT_AVAILABLE_DAY.getMessage());
             return visitDay();
         }
     }
     public static OrderMenu orderMenu() {
         OutputView.printOrderMenuAndCounted();
         String userInput1 = InputView.userInput();
+        OutputView.printShowEvent();
         OrderMenu orderMap = foodOrder(userInput1);
         OutputView.printOrderMenuAndCounted(orderMap.getOrderMap());
         return orderMap;
     }
 
     public static OrderMenu foodOrder(String order) {
+        try {
+            return getOrderMenu(order);
+        } catch (IllegalArgumentException e) {
+            System.out.println(ErrorMessage.USER_INSERT_NOT_AVAILABLE_MENU.getMessage());
+            return foodOrder(InputView.userInput());
+        }
+    }
+
+    private static OrderMenu getOrderMenu(String order) {
         Map<Menu, Integer> orderMap = new HashMap<>();
         for (String item : order.split(",")) {
             String[] parts = item.split("-");
             if (parts.length != 2) {
-                throw new IllegalArgumentException("매뉴와 갯수를 입력해주세요");
+                throw new IllegalArgumentException(ErrorMessage.USER_INSERT_MENU_AND_COUNT.getMessage());
             }
-            Menu menu = Menu.findByName(parts[0].trim());
-            int count = Integer.parseInt(parts[1].trim());
-
-            if (orderMap.containsKey(menu)) {
-                throw new IllegalArgumentException("같은 매뉴를 입력했습니다");
-            }
+            Menu menu = getMenu(parts, orderMap);
+            int count = getCount(parts);
             orderMap.put(menu, count);
         }
         return new OrderMenu(orderMap);
     }
 
+    private static int getCount(String[] parts) {
+        int count = Integer.parseInt(parts[1].trim());
+        return count;
+    }
+
+    private static Menu getMenu(String[] parts, Map<Menu, Integer> orderMap) {
+        Menu menu = Menu.findByName(parts[0].trim());
+        if (orderMap.containsKey(menu)) {
+            throw new IllegalArgumentException(ErrorMessage.USER_INSERT_DUPLICATION_MENU.getMessage());
+        }
+        return menu;
+    }
 
 }
