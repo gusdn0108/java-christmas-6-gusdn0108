9a4937ffd7fbd88ee0ce6197eda5ffdc9a34d05e
gusdn0108 <gusdn6671@naver.com>
2023-11-14 02:00:23 +0900

    refactor (Controller) : 평일/주말/특별 할인 수정

--- src/main/java/christmas/controller/Controller.java
+++ src/main/java/christmas/controller/Controller.java
@@ -6,6 +6,9 @@ import christmas.domain.MenuCategory;
 import christmas.domain.OrderMenu;
 import christmas.domain.Sale;
 import christmas.domain.VisitDay;
+import christmas.domain.sale.SpecialDaySale;
+import christmas.domain.sale.WeekdaySale;
+import christmas.domain.sale.WeekendDaySale;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 import java.util.ArrayList;
@@ -17,17 +20,21 @@ import java.util.Objects;
 
 public class Controller {
     public void run() {
-        VisitDay visitDay = visitDay();
+        VisitDay visitDay = InputView.visitDay();
         OrderMenu orderMenu = InputView.orderMenu();
         int totalPrice = orderMenu.totalPrice();
         OutputView.printTotalPrice(totalPrice);
         Sale salePrice = Sale.totalDaySalePrice(visitDay.getDay());
         OutputView.printPromotionPrice();
         OutputView.printDaySalePrice(salePrice.getSale());
-        int weekdayPrice = weekDayPrice(visitDay, orderMenu);
-        int weekEndPrice = weekEndPrice(visitDay, orderMenu);
-        int specialDayPrice = specialDayPrice(visitDay);
+
+
+        int weekdayPrice = new WeekdaySale().discount(visitDay,orderMenu);
+        int weekEndPrice = new WeekendDaySale().discount(visitDay,orderMenu);
+        int specialDayPrice = new SpecialDaySale().discount(visitDay,orderMenu);
         int promotionPrice = promotionPrice(totalPrice);
+
+
         int totalSalePrice = weekdayPrice + weekEndPrice + specialDayPrice + promotionPrice + salePrice.getSale();
         OutputView.printTotalSalePrice(totalSalePrice);
         int amountTotalPrice = totalPrice - totalSalePrice;
@@ -48,79 +55,6 @@ public class Controller {
     }
 
 
-    private int specialDayPrice(VisitDay visitDay) {
-        List<Integer> specialDays = List.of(3, 10, 17, 24, 25, 31);
-        if (visitDay.isContained(specialDays)) {
-            final int discount = 1000;
-            OutputView.printSpecialDay(discount);
-        }
-        return 0;
-    }
-
-    private int weekEndPrice(VisitDay visitDay, OrderMenu orderMenu) {
-        int weekdaySalePrice = 0;
-        if (visitDay.isWeekend()) {
-            weekdaySalePrice = weekendSale(visitDay, orderMenu);
-            if (weekdaySalePrice != 0) {
-                OutputView.printSaleWeekend(weekdaySalePrice);
-            }
-        }
-
-        return weekdaySalePrice;
-    }
-
-
-    private int weekDayPrice(VisitDay visitDay, OrderMenu orderMenu) {
-        int weekdaySalePrice = 0;
-        if (visitDay.isWeekday()) {
-            weekdaySalePrice = weekdaySale(visitDay, orderMenu);
-            OutputView.printSaleWeekDay(weekdaySalePrice);
-        }
-
-        return weekdaySalePrice;
-    }
-
-    private int weekendSale(VisitDay visitDay, OrderMenu orderMenu) {
-        Map<Menu, Integer> orderMap = orderMenu.getOrderMap();
-        int mainDiscount = 2023;
-        int discountPrice = 0;
-        if (visitDay.isWeekend()) {
-            for (Entry<Menu, Integer> entry : orderMap.entrySet()) {
-                Menu menu = entry.getKey();
-                int count = entry.getValue();
-                if (menu.getMenuCategory() == MenuCategory.Main) {
-                    discountPrice += mainDiscount * count;
-                }
-            }
-        }
-        return discountPrice;
-    }
-
-    private int weekdaySale(VisitDay visitDay, OrderMenu orderMenu) {
-        int dessertDiscount = 2023;
-        int discountPrice = 0;
-        if (visitDay.isWeekday()) {
-            for(Menu categroyMenu :orderMenu.getMenuInCategory(MenuCategory.Dessert)){
-                int count = orderMenu.getOrderCount(categroyMenu);
-                discountPrice += dessertDiscount * count;
-            }
-
-        }
-        return discountPrice;
-    }
-
-
-
-    private static VisitDay visitDay() {
-        try {
-            OutputView.printEventStart();
-            OutputView.printExpectationVisited();
-            String userInput = InputView.userInput();
-            return new VisitDay(InputView.parsedInputNumber(userInput));
-        } catch (NumberFormatException e) {
-            System.out.println("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
-            return visitDay();
-        }
     }
 
 
