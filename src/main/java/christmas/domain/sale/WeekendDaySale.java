58f8423e97e275e888335f672d3d25260e1338ab
gusdn0108 <gusdn6671@naver.com>
2023-11-14 01:50:31 +0900

    refactor (Controller => WeekendSale) : 주말 할인 메소드 수정

--- src/main/java/christmas/domain/sale/WeekendDaySale.java
+++ src/main/java/christmas/domain/sale/WeekendDaySale.java
@@ -4,8 +4,17 @@ import christmas.domain.Menu;
 import christmas.domain.MenuCategory;
 import christmas.domain.OrderMenu;
 import christmas.domain.VisitDay;
+import christmas.view.OutputView;
 
 public class WeekendDaySale {
+    public int discount(VisitDay visitDay, OrderMenu orderMenu) {
+        int weekendSalePrice = 0;
+        if (visitDay.isWeekend()) {
+            weekendSalePrice = weekendSale(visitDay, orderMenu);
+            OutputView.printSaleWeekend(weekendSalePrice);
+        }
+        return weekendSalePrice;
+    }
 
     public int weekendSale(VisitDay visitDay, OrderMenu orderMenu) {
         int mainDiscount = 2023;
