1c6c756884a32612fd4f855de282fe7ecccdfe62
gusdn0108 <gusdn6671@naver.com>
2023-11-14 01:41:45 +0900

    refactor (Controller => InputView) : foodOrder,orderMenu 이동

--- src/main/java/christmas/controller/Controller.java
+++ src/main/java/christmas/controller/Controller.java
@@ -18,7 +18,7 @@ import java.util.Objects;
 public class Controller {
     public void run() {
         VisitDay visitDay = visitDay();
-        OrderMenu orderMenu = orderMenu();
+        OrderMenu orderMenu = InputView.orderMenu();
         int totalPrice = orderMenu.totalPrice();
         OutputView.printTotalPrice(totalPrice);
         Sale salePrice = Sale.totalDaySalePrice(visitDay.getDay());
@@ -100,27 +100,16 @@ public class Controller {
         int dessertDiscount = 2023;
         int discountPrice = 0;
         if (visitDay.isWeekday()) {
-            Map<Menu, Integer> orderMap = orderMenu.getOrderMap();
-            for (Entry<Menu, Integer> entry : orderMap.entrySet()) {
-                Menu menu = entry.getKey();
-                List<Menu> menuCount = new ArrayList<>();
-                menuCount.add(menu);
-                if (menu.getMenuCategory() == MenuCategory.Dessert) {
-                    discountPrice += dessertDiscount * menuCount.size();
-                }
+            for(Menu categroyMenu :orderMenu.getMenuInCategory(MenuCategory.Dessert)){
+                int count = orderMenu.getOrderCount(categroyMenu);
+                discountPrice += dessertDiscount * count;
             }
+
         }
         return discountPrice;
     }
 
 
-    private OrderMenu orderMenu() {
-        OutputView.printOrderMenuAndCounted();
-        String userInput1 = InputView.userInput();
-        OrderMenu orderMap = foodOrder(userInput1);
-        OutputView.printOrderMenuAndCounted(orderMap.getOrderMap());
-        return orderMap;
-    }
 
     private static VisitDay visitDay() {
         try {
@@ -134,23 +123,6 @@ public class Controller {
         }
     }
 
-    private OrderMenu foodOrder(String order) {
-        Map<Menu, Integer> orderMap = new HashMap<>();
-        for (String item : order.split(",")) {
-            String[] parts = item.split("-");
-            if (parts.length != 2) {
-                throw new IllegalArgumentException("매뉴와 갯수를 입력해주세요");
-            }
-            Menu menu = Menu.findByName(parts[0].trim());
-            int count = Integer.parseInt(parts[1].trim());
-
-            if (orderMap.containsKey(menu)) {
-                throw new IllegalArgumentException("같은 매뉴를 입력했습니다");
-            }
-            orderMap.put(menu, count);
-        }
-        return new OrderMenu(orderMap);
-    }
 
 
 }
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,6 +1,10 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.domain.Menu;
+import christmas.domain.OrderMenu;
+import java.util.HashMap;
+import java.util.Map;
 
 public class InputView {
 
@@ -11,4 +15,32 @@ public class InputView {
     public static int parsedInputNumber(String input){
         return Integer.parseInt(input);
     }
+
+    public static OrderMenu orderMenu() {
+        OutputView.printOrderMenuAndCounted();
+        String userInput1 = InputView.userInput();
+        OrderMenu orderMap = foodOrder(userInput1);
+        OutputView.printOrderMenuAndCounted(orderMap.getOrderMap());
+        return orderMap;
+    }
+
+    public static OrderMenu foodOrder(String order) {
+        Map<Menu, Integer> orderMap = new HashMap<>();
+        for (String item : order.split(",")) {
+            String[] parts = item.split("-");
+            if (parts.length != 2) {
+                throw new IllegalArgumentException("매뉴와 갯수를 입력해주세요");
+            }
+            Menu menu = Menu.findByName(parts[0].trim());
+            int count = Integer.parseInt(parts[1].trim());
+
+            if (orderMap.containsKey(menu)) {
+                throw new IllegalArgumentException("같은 매뉴를 입력했습니다");
+            }
+            orderMap.put(menu, count);
+        }
+        return new OrderMenu(orderMap);
+    }
+
+
 }
