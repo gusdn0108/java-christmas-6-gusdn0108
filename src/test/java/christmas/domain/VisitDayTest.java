edc0c6d259baf36dc2cb9bc3f594b51932e12aa6
gusdn0108 <gusdn6671@naver.com>
2023-11-14 04:51:07 +0900

    test(VisitDay) : 지정한 날짜가 주말이 맞는지 확인하기

---
+++ src/test/java/christmas/domain/VisitDayTest.java
@@ -0,0 +1,18 @@
+package christmas.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class VisitDayTest {
+    @DisplayName("지정한 날짜가 주말이 맞는지 확인하기")
+    @Test
+    void testIsWeekend() {
+        VisitDay weekendDay = new VisitDay(1);
+        assertTrue(weekendDay.isWeekend());
+    }
+
+
+}
