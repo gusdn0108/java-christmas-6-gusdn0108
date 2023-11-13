2eda9e996e53deeabb3731b5b9660fcc99d4d0ca
gusdn0108 <gusdn6671@naver.com>
2023-11-14 04:50:21 +0900

    test(OrderMenu) : 특정 카테고리 매뉴 찾기

--- src/test/java/christmas/domain/OrderMenuTest.java
+++ src/test/java/christmas/domain/OrderMenuTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class OrderMenuTest {
-
     @DisplayName("주문한 매뉴 가격 구하기")
     @Test
     void testTotalPrice() {
@@ -20,4 +19,15 @@ class OrderMenuTest {
         assertEquals(145000, orderMenu.totalPrice());
     }
 
+    @DisplayName("특정 카테고리 매뉴 찾기")
+    @Test
+    void testGetMenuInCategory() {
+        Map<Menu, Integer> orderMap = new HashMap<>();
+        orderMap.put(Menu.REDWAIN, 2);
+        orderMap.put(Menu.CHRISMASFASTA, 1);
+        orderMap.put(Menu.ICECREAM, 3);
+
+        OrderMenu orderMenu = new OrderMenu(orderMap);
+        assertEquals(1, orderMenu.getMenuInCategory(MenuCategory.Dessert).size());
+    }
 }
