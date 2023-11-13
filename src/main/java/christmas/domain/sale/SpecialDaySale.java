289ea644467a791ae3d2a56a4a0840eea018725b
gusdn0108 <gusdn6671@naver.com>
2023-11-14 01:54:02 +0900

    refactor (Controller => SpecialDaySale) : 특별할인 클래스 생성 및 주말 할인 메소드 이동

---
+++ src/main/java/christmas/domain/sale/SpecialDaySale.java
@@ -0,0 +1,17 @@
+package christmas.domain.sale;
+
+import christmas.domain.VisitDay;
+import christmas.view.OutputView;
+import java.util.List;
+
+public class SpecialDaySale {
+    private static final List<Integer> SPECIAL_DAY = List.of(3, 10, 17, 24, 25, 31);
+    private int discount (VisitDay visitDay) {
+        if (visitDay.isContained(SPECIAL_DAY)) {
+            final int discount = 1000;
+            OutputView.printSpecialDay(discount);
+        }
+        return 0;
+    }
+
+}
