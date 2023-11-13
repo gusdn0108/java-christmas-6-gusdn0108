67a85f0ddab4ce34f2b71ceda22de27f24d310dd
gusdn0108 <gusdn6671@naver.com>
2023-11-13 23:50:32 +0900

    feat(VisitDay) : 평일/주말 나누기

--- src/main/java/christmas/domain/VisitDay.java
+++ src/main/java/christmas/domain/VisitDay.java
@@ -1,13 +1,32 @@
 package christmas.domain;
 
+import christmas.view.OutputView;
+
 public class VisitDay {
     private final int day;
 
     public VisitDay(int day) {
-        if(day < 1 || day > 31){
-            throw new IllegalArgumentException("1-31이하 만 가능합니다");
+            validate(day);
+            this.day = day;
+    }
+
+    private static void dayRange(int day) {
+        if (day < 1 || day > 31) {
+            throw new IllegalArgumentException("[ERROR] 날짜는 1-31 까지만 입력 가능합니다");
         }
-        this.day = day;
+    }
+
+    private void validate(int day){
+        dayRange(day);
+    }
+
+    public String getDayType(int day) {
+        int dayOfWeek = (day + 5) % 7;
+        String dayType = "주말";
+        if (dayOfWeek >= 1 && dayOfWeek <= 5) {
+            dayType = "평일";
+        }
+        return dayType;
     }
 
     public int getDay() {
