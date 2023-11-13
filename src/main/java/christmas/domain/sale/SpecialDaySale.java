ce076869a4b2ba33d78cd396a2fcba36b601e168
gusdn0108 <gusdn6671@naver.com>
2023-11-14 03:55:10 +0900

    refactor (SpecicalDaySale) : SPECIAL_DISCOUNT 상수화

--- src/main/java/christmas/domain/sale/SpecialDaySale.java
+++ src/main/java/christmas/domain/sale/SpecialDaySale.java
@@ -7,9 +7,10 @@ import java.util.List;
 
 public class SpecialDaySale implements Sale {
     private static final List<Integer> SPECIAL_DAYS = List.of(3, 10, 17, 24, 25, 31);
+    private static final int SPECIAL_DISCOUNT = 1000;
     public int discount (VisitDay visitDay, OrderMenu orderMenu) {
         if (visitDay.isContained(SPECIAL_DAYS)) {
-            int discount = 1000;
+            int discount = SPECIAL_DISCOUNT;
             OutputView.printSpecialDay(discount);
             return discount;
         }
