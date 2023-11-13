344b2cd62734ad5c9951f11af27e65d865906ab5
gusdn0108 <gusdn6671@naver.com>
2023-11-14 04:51:18 +0900

    test(VisitDay) : 지정한 날짜가 평일이 맞는지 확인하기

--- src/test/java/christmas/domain/VisitDayTest.java
+++ src/test/java/christmas/domain/VisitDayTest.java
@@ -14,5 +14,13 @@ class VisitDayTest {
         assertTrue(weekendDay.isWeekend());
     }
 
+    @DisplayName("지정한 날짜가 평일이 맞는지 확인하기")
+    @Test
+    void testIsWeekday() {
+        VisitDay weekday = new VisitDay(3);
+        assertTrue(weekday.isWeekday());
+    }
+
+
 
 }
