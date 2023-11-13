63c867ffd16ae0dc396a4431e32a9de5d2c8d1b4
gusdn0108 <gusdn6671@naver.com>
2023-11-14 03:00:19 +0900

    feat (Controller) : 할인 / 증정이 없을 경우 추가

--- src/main/java/christmas/controller/Controller.java
+++ src/main/java/christmas/controller/Controller.java
@@ -4,7 +4,6 @@ import christmas.domain.Badge;
 import christmas.domain.OrderMenu;
 import christmas.domain.VisitDay;
 import christmas.domain.sale.DdaySale;
-import christmas.domain.sale.Sale;
 import christmas.domain.sale.Sales;
 import christmas.domain.sale.SpecialDaySale;
 import christmas.domain.sale.WeekdaySale;
@@ -18,12 +17,27 @@ public class Controller {
     public void run() {
         VisitDay visitDay = InputView.visitDay();
         OrderMenu orderMenu = InputView.orderMenu();
-
         int totalPrice = orderMenu.totalPrice();
-        OutputView.printTotalPrice(totalPrice);
 
-        OutputView.printPromotionPrice();
+        if(totalPrice > 120000){
+            OutputView.promotionMenu();
+            OutputView.printGiveAwayChampagne();
+        }
+
+        OutputView.printBeforeSaleTotalPrice(totalPrice);
+
+        if(totalPrice < 120000){
+            OutputView.promotionMenu();
+            OutputView.printNoting();
+        }
+
+        OutputView.printBenefitDetails();
         int discountPrice = discountPrice(visitDay, orderMenu);
+
+
+        if(totalPrice < 10000){
+            OutputView.printNoting();
+        }
         int promotionPrice = promotionPrice(totalPrice);
         OutputView.printTotalSalePrice(discountPrice + promotionPrice);
 
@@ -31,14 +45,22 @@ public class Controller {
         OutputView.printAmountOfPayment(amountTotalPrice);
 
         Badge badge = Badge.awardBadge(amountTotalPrice);
-        OutputView.printEventBadge(badge);
+
+        if(discountPrice + promotionPrice < 5000){
+            OutputView.printPromotionBadgeEvent();
+            OutputView.printNoting();
+        }
+
+        if(discountPrice + promotionPrice > 5000){
+            OutputView.printEventBadge(badge);
+        }
     }
 
     private int promotionPrice(int totalPrice) {
         int gitEventDiscount = 0;
         if (totalPrice > 120000) {
             gitEventDiscount = 25000;
-            System.out.println("증정 이벤트: " + "-" + gitEventDiscount + "원");
+            OutputView.printGiveAwayEvent(gitEventDiscount);
         }
         return gitEventDiscount;
     }
